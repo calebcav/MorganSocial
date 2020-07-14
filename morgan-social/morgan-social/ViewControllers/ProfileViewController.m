439d6eff96830fb96e8b33fab5f81c803318a72d
calebcav <calebc@fb.com>
2020-07-14 18:14:59 -0400

    Able to upload picture to profile picture

--- morgan-social/morgan-social/ViewControllers/ProfileViewController.m
+++ morgan-social/morgan-social/ViewControllers/ProfileViewController.m
@@ -20,6 +20,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    PFUser *user = PFUser.currentUser;
+    self.profileUsername.text = user.username;
+    self.profilePicture.file = user[@"picture"];
+    [self.profilePicture loadInBackground];
+    UITapGestureRecognizer *tapPhoto = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hello:)];
+    [tapPhoto setDelegate:self];
+    self.profilePicture.userInteractionEnabled = YES;
+    [self.profilePicture addGestureRecognizer:tapPhoto];
+    
+}
+
+- (void) hello:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    PFUser *user = PFUser.currentUser;
+    user[@"picture"] = [self getPFFileFromImage:editedImage];
+    user.saveInBackground;
+    self.profilePicture.file = user[@"picture"];
+    [self.profilePicture loadInBackground];
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+    
+    if (!image){
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    
+    if (!imageData) {
+        return nil;
+    }
+    
+    return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
 }
 
 /*
