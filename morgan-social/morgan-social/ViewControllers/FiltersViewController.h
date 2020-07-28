ba203cfb59040cf69352d406a709ec2f14a5877e
calebcav <calebc@fb.com>
2020-07-28 18:17:10 -0400

    Users can now filter posts

---
+++ morgan-social/morgan-social/ViewControllers/FiltersViewController.h
@@ -0,0 +1,17 @@
+//
+//  FiltersViewController.h
+//  morgan-social
+//
+//  Created by Caleb Caviness on 7/28/20.
+//  Copyright © 2020 Caleb Caviness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FiltersViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ morgan-social/morgan-social/ViewControllers/FiltersViewController.m
@@ -0,0 +1,32 @@
+//
+//  FiltersViewController.m
+//  morgan-social
+//
+//  Created by Caleb Caviness on 7/28/20.
+//  Copyright © 2020 Caleb Caviness. All rights reserved.
+//
+
+#import "FiltersViewController.h"
+
+@interface FiltersViewController ()
+
+@end
+
+@implementation FiltersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
